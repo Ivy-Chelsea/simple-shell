e795cb408ffcddd78e1288bd5309d27d5ed662d9
Ivy-Chelsea <ivychelsea20@gmail.com>
2022-08-03 09:58:08 -0700

    output

---
+++ output.c
@@ -0,0 +1,22 @@
+nclude "shell.h"
+/**
+ * _prerror - prints custom error
+ * @argv: programme name
+ * @c: error count
+ * @cmd: command
+ * Return: void
+ */
+void _prerror(char **argv, int c, char **cmd)
+{
+	char *er = _itoa(c);
+
+	PRINTER(argv[0]);
+	PRINTER(" : ");
+	PRINTER(er);
+	PRINTER(": ");
+	PRINTER(cmd[0]);
+	PRINTER(": Illegal number: ");
+	PRINTER(cmd[1]);
+	PRINTER("\n");
+	free(er);
+}
