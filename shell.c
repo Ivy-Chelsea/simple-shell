a522fd8146f36d0765a608d9e9f994d06371faea
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-08-04 21:29:55 +0300

    shell

--- shell.c
+++ shell.c
@@ -34,14 +34,14 @@ int main(__attribute__((unused)) int argc, char **argv)
 		else if (check_builtin(cmd) == 0)
 		{
 			st = handle_builtin(cmd, st);
-			free(cmd, input);
+			free_all(cmd, input);
 			continue;
 		}
 		else
 		{
 			st = check_cmd(cmd, input, counter, argv);
 		}
-		free(cmd, input);
+		free_all(cmd, input);
 	}
 	return (statue);
 }
