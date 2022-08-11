db6b5b95b3116dfcaf50fba952b85dfed9e39b7d
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-08-11 22:57:49 +0300

    fix1

--- builtins.c
+++ builtins.c
@@ -21,7 +21,7 @@ void  exit_bul(char **cmd, char *input, char **argv, int c)
 	{
 		if (_isalpha(cmd[1][i++]) != 0)
 		{
-			prerror(argv, c, cmd);
+			_prerror(argv, c, cmd);
 			break;
 		}
 		else
